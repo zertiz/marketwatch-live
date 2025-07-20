23c60a0251da17121d246553fb2aa02323cfa95e
zertiz <149271820+zertiz@users.noreply.github.com>
2025-07-20 21:48:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ async function fetchNews() {
   try {
     console.log("fetchNews appelée !");
     const proxyUrl = 'https://api.allorigins.win/get?url=' +
-      encodeURIComponent('https://www.zonebourse.com/rss/news.xml');
+      encodeURIComponent('https://www.marketwatch.com/rss/topstories');
 
     const res = await fetch(proxyUrl);
     const data = await res.json();
